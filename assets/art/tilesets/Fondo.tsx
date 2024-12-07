e9b11b42621f2beb47ca0b7601db79a41d00796a
Bloodbay8 <153459461+Bloodbay8@users.noreply.github.com>
2024-12-06 22:03:45 -0400

    Mapa con colisiones y Kirby s

---
+++ assets/art/tilesets/Fondo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Fondo" tilewidth="24" tileheight="24" tilecount="220" columns="20">
+ <image source="./Background_2.png" trans="ff00ff" width="496" height="272"/>
+</tileset>
