5041b3cce0f87ec0c23eb48c555797f91a17ddab
Bloodbay8 <153459461+Bloodbay8@users.noreply.github.com>
2025-01-06 18:40:21 -0400

    Sprites de Fire Kirby, Beam Kirby y Ataques personalizados de cada uno con Sprites. Entidad Kirby completada y Enemigos Completados

--- core/src/main/java/KirbyGame_HVL/git/entities/player/Box2dPlayer.java
+++
@@ -1,8 +0,0 @@
-package KirbyGame_HVL.git.entities.player;
-
-import com.badlogic.gdx.physics.box2d.World;
-
-public interface Box2dPlayer {
-
-    void createBody (World world, float x, float y);
-}
